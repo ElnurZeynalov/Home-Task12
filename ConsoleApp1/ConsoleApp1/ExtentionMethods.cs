ddabe9af65c98f5cf3486b391cf3c9c3cc4582f2
ElnurZeynalov <tu74449ce@code.edu.az>
2022-03-29 11:43:07 +0400

    Task 1 Complete

---
+++ ConsoleApp1/ConsoleApp1/ExtentionMethods.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal static class ExtentionMethods
+    {
+        public static bool Odd(this int value)
+        {
+            if (value % 2 != 0)
+                return true;
+            return false;
+        }
+        public static bool Even(this int value)
+        {
+            if (value % 2 == 0)
+                return true;
+            return false;
+        }
+        public static bool IsContainsDigit(this string str)
+        {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (char.IsDigit(str[i]))
+                        return true;
+                }
+            return false;
+        }
+        public static void ToCapitalize(this string str)
+        {
+                str = str.ToLower();
+                Console.WriteLine(char.ToUpper(str[0]) + str.Substring(1));
+            
+        }
+        public static int[] GetValueIndexes(this string str , char chr)
+        {
+            int[] arr = new int[0];
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (str[i] == chr)
+                    {
+                        Array.Resize(ref arr, arr.Length+1);
+                        arr[arr.Length-1] = i;
+                    }
+                }
+                return arr;                
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(value.Odd());
+
+            int value2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(value2.Even());
+
+            string str = Console.ReadLine();
+            Console.WriteLine(str.IsContainsDigit());
+
+            string str2 = Console.ReadLine();
+            str2.ToCapitalize();
+
+            string str3 = Console.ReadLine();
+            char chr = Convert.ToChar(Console.ReadLine());
+            foreach (var item in str3.GetValueIndexes(chr))
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
